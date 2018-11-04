ab9ca63546801873826d3adb0d01608bfb5407a7
Gustav Wengel <gustavwengel@gmail.com>
2018-11-04 16:09:03 +0100

    assignment one babyy

--- src/HeroList.js
+++ src/HeroList.js
@@ -4,33 +4,25 @@ import { withScroll } from 'react-fns';
 import { calculateBackgroundColor } from './not-important/utils';
 import { FormController } from './not-important/FormController';
 
-class HeroListClass extends Component {
+export class HeroList extends Component {
 	state = {
 		heroes: initialHeroes
 	};
 	
 	render() {
-		const sortedHeroes = [... this.state.heroes]
+		const sortedHeroes = [...this.state.heroes]
 			.sort((hero, secondHero) => hero.evil - secondHero.evil);
 		
 		const heroComponents = sortedHeroes
 			.map(hero => <li className="hero-list-item" key={hero.name}>{hero.evil} - {hero.name}</li>)
 		
 		return (
-				<div style={calculateBackgroundColor(document.body.clientHeight, this.props.y)}>
-					<ul>
-						<li className="hero-list-item hero-list-header">Evil score - Hero name</li>
-						{heroComponents}
-					</ul>
-					
-					<form onSubmit={(e) => e.preventDefault()}>
-						<input type="number"/>
-						<input type="text" />
-						<button>Create</button>
-					</form>
-				</div>
-			);
+			<div>
+				<ul>
+					<li className="hero-list-item hero-list-header">Evil score - Hero name</li>
+					{heroComponents}
+				</ul>
+			</div>
+		);
 	}
 }
-
-export const HeroList = withScroll(HeroListClass);
---
+++ src/assignments/assignment-1.js
@@ -0,0 +1,18 @@
+
+// Welcome to the first exercise!
+
+/*
+EXERCISE A:
+Let's get the app running. Inside your terminal type "npm run part1" when in the root directory of the
+workshop, or go into the part1 folder and type "npm run start"
+
+You should see a list of superheroes pop up in your browser at localhost:3000
+*/
+
+/*
+
+
+ */
+
+
+
