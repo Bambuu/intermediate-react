09dbeef802eef7dc0e963315441569090de41764
Jeppe Reinhold <jeppereinhold@gmail.com>
2018-11-04 17:18:44 +0100

    initial slides

---
+++ slides/tester.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Tester = (props) => {
+	return <p>{[<code>cool</code>, <code>beans</code>]}</p>
+}
---
+++ slides/theme.js
@@ -0,0 +1,20 @@
+import { dark } from "mdx-deck/themes";
+import okaidia from "react-syntax-highlighter/styles/prism/okaidia";
+import baseTheme from "mdx-deck/themes";
+
+export default {
+  ...baseTheme,
+  colors: {
+    ...baseTheme.colors,
+    text: "#d6deeb",
+    background: "#011627",
+    link: "#addb67",
+    code: "rgb(199, 146, 234)"
+  }
+};
+// export default {
+// 	...dark,
+// 	prism: {
+// 		style: okaidia
+// 	}
+// }
