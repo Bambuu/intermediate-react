27c2feae04df008f2ce136f11a1ef0e491a5dd48
geewee <gustav@bambuu.dk>
2018-11-12 19:36:43 +0100

    part1

---
+++ part1/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './not-important/index.css';
+
+ReactDOM.render(<h1>Hello Intermediate React!</h1>, document.getElementById('root'));
