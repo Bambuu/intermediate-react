c5cefa931bcd117d6fe3d030c84636d060e2e818
geewee <gustav@bambuu.dk>
2018-11-12 21:02:03 +0100

    assignment 1

--- part4/src/assignments/assignment-1.js
+++ part4/src/assignments/assignment-1.js
@@ -1,1 +1,22 @@
 // ASSIGNMENT 1 - ????????????????????
+
+
+/*
+
+
+Exercises:
+  - Install routing. WE INSTALL THIS
+    - notice urls and extra exercises
+
+  - Sidebar works with links
+
+  - Top Level Router
+
+
+  - Header works with current location
+
+
+
+
+
+ */
