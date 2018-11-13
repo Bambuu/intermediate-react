af79971adf50ed6c6e6689bee50509003b4d9c1f
geewee <gustav@bambuu.dk>
2018-11-13 17:06:22 +0100

    made the thing

--- part4/src/SideBar.js
+++ part4/src/SideBar.js
@@ -6,14 +6,14 @@ import {Link} from 'react-router-dom';
 export const SideBar = (props) => {
 	return (
 		<div className="sidebar">
-			{/*<Link to={"/heroes"} />*/}
-      {/*<Link to={"/movies"} />*/}
-      {/*<Link to={"/theming"} />*/}
-      {/*<Link to={"/extra"} />*/}
-			<a onClick={() => props.changePage('/')}>Heroes</a>
-			<a onClick={() => props.changePage('/movies')}>Movies</a>
-      <a onClick={() => props.changePage('/theming')}>Theming</a>
-      <a onClick={() => props.changePage('/extra')}>Extra Exercise</a>
+			<Link to={"/heroes"} onClick={() => props.changePage('/')}>Heroes</Link>
+			<Link to={"/movies"} onClick={() => props.changePage('/movies')} >Movies</Link>
+			<Link to={"/theming"} onClick={() => props.changePage('/theming')} >Theming</Link>
+			<Link to={"/extra"} onClick={() => props.changePage('/extra')} >Extra Exercise</Link>
+			{/*<a onClick={() => props.changePage('/')}>Heroes</a>*/}
+			{/*<a onClick={() => props.changePage('/movies')}>Movies</a>*/}
+      {/*<a onClick={() => props.changePage('/theming')}>Theming</a>*/}
+      {/*<a onClick={() => props.changePage('/extra')}>Extra Exercise</a>*/}
 		</div>)
 };
 
--- part4/src/assignments/assignment-1.js
+++ part4/src/assignments/assignment-1.js
@@ -6,7 +6,30 @@
 /*
 
 
+
 Exercises:
+
+  - Add BrowserRouter in ap
+  - Only one child allowed
+  - Put everything in a div
+
+  - Add links to sidebar with urls
+     - keep onClick
+     - url changes now! Hallelujah!
+     - But refresh..??
+
+  - Add Routes
+  - Components..?
+   - Use both component and render
+
+  - Links work now - but what about base-link?! /
+  - Redirect .. !
+  - Default Route..???!
+
+
+
+
+
   - Install routing. WE INSTALL THIS
     - notice urls and extra exercises
 
