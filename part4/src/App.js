2908b7f8596a48f3e11f86cd6361b46bbf161f56
Jeppe Reinhold <jeppereinhold@gmail.com>
2018-11-13 19:40:35 +0100

    format app.js 4

--- part4/src/App.js
+++ part4/src/App.js
@@ -1,27 +1,45 @@
-import React, { Component } from 'react';
-import { SideBar } from './SideBar';
-import { Header } from './Header';
-import { HeroList } from './HeroList';
+import React, { Component } from "react";
+import { SideBar } from "./SideBar";
+import { Header } from "./Header";
+import { HeroList } from "./HeroList";
 import { ExtraAssignment } from "./ExtraAssignment";
 
 class App extends Component {
   state = {
-    currentPage: '/'
+    currentPage: "/"
   };
 
-  changePage = (newPage) => {
-    this.setState({currentPage: newPage})
+  changePage = newPage => {
+    this.setState({ currentPage: newPage });
   };
 
   render() {
     // Default component is HeroList
-    let mainComponent = <div className="hero-list"><HeroList/></div>;
-    if (this.state.currentPage === '/movies'){
-      mainComponent = <div className="tall-div">Here's a fun list of superhero movies.. to be implemented later..</div>
-    } else if (this.state.currentPage === '/theming'){
-      mainComponent = <div>Here we'll end up changing the theme of our app.. later</div>
-    } else if (this.state.currentPage === '/extra'){
-      mainComponent = <div><ExtraAssignment prop1="oh my" prop2="oh me"/></div>
+    let mainComponent = (
+      <div className="hero-list">
+        <HeroList />
+      </div>
+    );
+    if (this.state.currentPage === "/movies") {
+      mainComponent = (
+        <div className="tall-div">
+          Here's a fun list of superhero movies.. to be
+          implemented later..
+        </div>
+      );
+    } else if (this.state.currentPage === "/theming") {
+      mainComponent = (
+        <div>
+          Here we'll end up changing the theme of our app..
+          later
+        </div>
+      );
+    } else if (this.state.currentPage === "/extra") {
+      mainComponent = (
+        <div>
+          <ExtraAssignment prop1="oh my" prop2="oh me" />
+        </div>
+      );
     }
     return (
       <div className="root">
