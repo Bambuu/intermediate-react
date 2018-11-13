ce8049d1989dea18b2b9eea255ad036c96aa5aa7
geewee <gustav@bambuu.dk>
2018-11-13 16:44:30 +0100

    made the thing

--- part4/src/SideBar.js
+++ part4/src/SideBar.js
@@ -1,10 +1,15 @@
 /* eslint-disable */
 import React from 'react';
 import PropTypes from "prop-types";
+import {Link} from 'react-router-dom';
 
 export const SideBar = (props) => {
 	return (
 		<div className="sidebar">
+			{/*<Link to={"/heroes"} />*/}
+      {/*<Link to={"/movies"} />*/}
+      {/*<Link to={"/theming"} />*/}
+      {/*<Link to={"/extra"} />*/}
 			<a onClick={() => props.changePage('/')}>Heroes</a>
 			<a onClick={() => props.changePage('/movies')}>Movies</a>
       <a onClick={() => props.changePage('/theming')}>Theming</a>
