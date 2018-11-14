04f8bad69675875a23f4c6a1b96e8151d52c72f1
geewee <gustav@bambuu.dk>
2018-11-14 18:59:22 +0100

    part6

---
+++ part6/assignments/extra-assignment.js
@@ -0,0 +1,2 @@
+
+// extra - create a render prop..?
---
+++ part6/src/not-important/utils.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types'
+import * as React from "react";
+
+export const calculateBackgroundColor = (scrollPosition) => {
+  const percentageScrolled = scrollPosition * 1.5 / window.innerHeight;
+  return `rgb(255, 215, 0, ${percentageScrolled})`
+};
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+export function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+
+export class TestRenderProp extends React.Component {
+
+
+  render(){
+    return (<div style={{border: 'solid 2px red'}}>
+      {this.props.children("Hello from RenderProp")}
+    </div>)
+  }
+
+  static propTypes = {
+    children: PropTypes.func.isRequired
+  }
+}
