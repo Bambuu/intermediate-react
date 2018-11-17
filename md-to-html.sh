c7a74fd1b7869823f932db2c14d3b1c08be50c45
Jeppe Reinhold <jeppereinhold@gmail.com>
2018-11-17 09:33:57 +0100

    new assignmet folder

--- md-to-html.sh
+++ md-to-html.sh
@@ -1,18 +1,31 @@
-ASSPATH=./part1/src/assignments/assignment-
-./node_modules/.bin/github-markdown -s "../../../github-markdown.css" ${ASSPATH}1.md > ${ASSPATH}1.html
+TOOLPATH=./node_modules/.bin/github-markdown
+ARGUMENTS="-s ../../github-markdown.css"
 
-ASSPATH=./part2/src/assignments/assignment-
-./node_modules/.bin/github-markdown -s "../../../github-markdown.css" ${ASSPATH}1.md > ${ASSPATH}1.html
-./node_modules/.bin/github-markdown -s "../../../github-markdown.css" ${ASSPATH}2.md > ${ASSPATH}2.html
-./node_modules/.bin/github-markdown -s "../../../github-markdown.css" ${ASSPATH}extra-1.md > ${ASSPATH}extra-1.html
-./node_modules/.bin/github-markdown -s "../../../github-markdown.css" ${ASSPATH}extra-2.md > ${ASSPATH}extra-2.html
+ASSPATH=./part1/assignments/assignment-
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}1.md > ${ASSPATH}1.html
 
-ASSPATH=./part3/src/assignments/assignment-
-./node_modules/.bin/github-markdown -s "../../../github-markdown.css" ${ASSPATH}1.md > ${ASSPATH}1.html
-./node_modules/.bin/github-markdown -s "../../../github-markdown.css" ${ASSPATH}2.md > ${ASSPATH}2.html
-./node_modules/.bin/github-markdown -s "../../../github-markdown.css" ${ASSPATH}extra.md > ${ASSPATH}extra.html
+ASSPATH=./part2/assignments/assignment-
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}1.md > ${ASSPATH}1.html
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}2.md > ${ASSPATH}2.html
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}extra-1.md > ${ASSPATH}extra-1.html
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}extra-2.md > ${ASSPATH}extra-2.html
 
+ASSPATH=./part3/assignments/assignment-
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}1.md > ${ASSPATH}1.html
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}2.md > ${ASSPATH}2.html
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}extra.md > ${ASSPATH}extra.html
 
-ASSPATH=./part6/src/assignments/assignment-
-./node_modules/.bin/github-markdown -s "../../../github-markdown.css" ${ASSPATH}1.md > ${ASSPATH}1.html
-./node_modules/.bin/github-markdown -s "../../../github-markdown.css" ${ASSPATH}2.md > ${ASSPATH}2.html
+ASSPATH=./part4/assignments/assignment-
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}1.md > ${ASSPATH}1.html
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}extra-1.md > ${ASSPATH}extra-1.html
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}extra-2.md > ${ASSPATH}extra-2.html
+
+ASSPATH=./part5/assignments/assignment-
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}1.md > ${ASSPATH}1.html
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}2.md > ${ASSPATH}2.html
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}3.md > ${ASSPATH}3.html
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}extra.md > ${ASSPATH}extra.html
+
+ASSPATH=./part6/assignments/assignment-
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}1.md > ${ASSPATH}1.html
+${TOOLPATH} ${ARGUMENTS} ${ASSPATH}2.md > ${ASSPATH}2.html
