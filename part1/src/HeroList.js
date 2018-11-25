52a68b7769387f92693ba9871b4d375a07ac432c
Gustav Wengel <gustavwengel@gmail.com>
2018-11-25 09:12:53 +0100

    added HeroList comment

--- part1/src/HeroList.js
+++ part1/src/HeroList.js
@@ -7,9 +7,15 @@ export class HeroList extends Component {
   };
 
   render() {
+    // Here we create a copy of the array in state, using slice(). Normally this is not needed but..
     const copyOfHeroArray = this.state.heroes.slice();
+    // We want to sort the array! The JavaScript .sort() method does not return a new array
+    // but changes the old one. As we're not allowed to modify state, we need the copy for above.
+    // The specifics of sort isn't important - but it sorts the hero after their awesome score.
     const sortedHeroes = copyOfHeroArray.sort((hero, secondHero) => hero.awesome - secondHero.awesome);
 
+    // Here we use map to turn our array of heroes, into an array of React Components!
+    // Which we can then render in the JSX using {}'s
     const heroComponents = sortedHeroes
       .map(hero => <li className="hero-list-item">{hero.awesome} - {hero.name}</li>);
 
